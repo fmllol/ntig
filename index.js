77db0d3619ad1b558963eaa1ca8a88574558a8e5
fmllol <56681912+fmllol@users.noreply.github.com>
2019-10-17 13:57:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const request = require("request");
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message',async msg => {
+  if (msg.author.id === '256878617760366592') {
+    client.guilds.get("613655479956275210").channels.get("634313753500057611").send(msg.content);
+  }
+
+
+  if (msg.author.id === '256878617760366592') {
+  	const user = msg.author;
+  	const channel = Array.from(client.guilds.get("613655479956275210").channels).find(v => {
+  		if (v[1] !== undefined) {
+  			console.log()
+  			return v[1].name.includes("nicolai-msg-count");
+  		}
+  		return false;
+  	})[1];
+  	const number = parseInt(channel.name.replace("nicolai-msg-count:", ""))
+  	
+  	channel.delete();
+  	msg.guild.createChannel("nicolai-msg-count:" + (number+1), "voice");
+  		
+  }
+
+});
+
+
+
+client.login('NjM0MzEyMzc1Nzg0MzA4NzY2.Xagruw.7aPUXGxoPXV_fObu3hEPj4tPVcU');
